71b4a51d01f16eee34283112f7f90593932bcfde
Bojan Faletic <bojanf.1996@gmail.com>
2022-12-07 17:19:05 +0100

    all tests are passing

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,10 @@
-#include "loader.hpp"
-#include "parser.hpp"
-#include <gtest/gtest.h>
-
 #include <string>
 #include <vector>
+#include <gtest/gtest.h>
+
+#include "loader.hpp"
+#include "parser.hpp"
+
 using namespace std;
 
 void load_parser(Loader &ld, Parser &pa) {
@@ -25,6 +26,36 @@ TEST(Parser, add) {
 TEST(Parser, assign) {
   Parser pa;
   Loader ld{"../test_cases/assign.json"};
+
+  load_parser(ld, pa);
+  for (pair<string, int> item : ld.get()){
+    EXPECT_EQ(pa.parse(item.first), item.second);
+  }
+}
+
+TEST(Parser, branch) {
+  Parser pa;
+  Loader ld{"../test_cases/branch.json"};
+
+  load_parser(ld, pa);
+  for (pair<string, int> item : ld.get()){
+    EXPECT_EQ(pa.parse(item.first), item.second);
+  }
+}
+
+TEST(Parser, insert_new) {
+  Parser pa;
+  Loader ld{"../test_cases/insert_new.json"};
+
+  load_parser(ld, pa);
+  for (pair<string, int> item : ld.get()){
+    EXPECT_EQ(pa.parse(item.first), item.second);
+  }
+}
+
+TEST(Parser, full) {
+  Parser pa;
+  Loader ld{"../test_cases/full.json"};
 
   load_parser(ld, pa);
   for (pair<string, int> item : ld.get()){
